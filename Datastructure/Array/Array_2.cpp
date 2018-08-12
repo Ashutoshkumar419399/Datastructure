431d9f2e5ea1567e32342f469a84859651eeb86c
Ashutoshkumar419399 <ashutoshkumar419399@gmail.com>
2018-08-13 00:07:29 +0530

    Datastructure

---
+++ Datastructure/Array/Array_2.cpp
@@ -0,0 +1,8 @@
+# include <iostream>
+using namespace std;
+int main(){
+	int arr[5]={5,7,9,8,6};
+	for (int i=4; i>-1; i--)
+	cout<< arr[i]<<endl;
+	
+}
---
+++ Datastructure/Array/Ayyay_1.cpp
@@ -0,0 +1,9 @@
+# include<iostream>
+
+using namespace std;
+int main (){
+int arr[5]={5,4,7,9,6};
+for (int i=0; i<5; i++)
+cout<<(arr[i])<<endl;
+
+}
